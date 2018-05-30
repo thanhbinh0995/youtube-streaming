30403d54c618746bd3517d143f376290d006d519
Binh Pham <thanhbinh0995@gmail.com>
2018-05-30 10:00:08 +0700

    remove file

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 import UserController from './user-controller';
-import BinanceController from './binance-controller';
 
 module.exports = {
     userController: new UserController(),
-    binanceController: new BinanceController(),
 };
--- server/repositories/index.js
+++ server/repositories/index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 import UserRepository from './user-repository';
-import BinanceRepository from './binance-repository';
 
 module.exports = {
     userRepository: new UserRepository(),
-    binanceRepository: new BinanceRepository(),
 };
